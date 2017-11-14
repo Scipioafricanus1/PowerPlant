a6978bdc1ab9a26fe0a041128d2510d6f5e73862
MonicaTran <monica.k.tran@gmail.com>
2017-11-14 10:42:55 -0600

    Added table, added segues, and nav controller.

---
+++ PowerPlant/PlantCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantCell.swift
+//  PowerPlant
+//
+//  Created by Ubicomp7 on 11/14/17.
+//  Copyright © 2017 PowerPlantTeam. All rights reserved.
+//
+
+import UIKit
+
+class PlantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

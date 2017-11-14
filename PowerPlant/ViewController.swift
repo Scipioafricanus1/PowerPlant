8005521cb2ac12e0652dcdefa31fa34ba0959f40
Scipioafricanus1 <bdharris3@uh.edu>
2017-11-14 10:03:35 -0600

    This is the current project without anything done yet.
    current project.

---
+++ PowerPlant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PowerPlant
+//
+//  Created by Ubicomp7 on 11/13/17.
+//  Copyright © 2017 PowerPlantTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

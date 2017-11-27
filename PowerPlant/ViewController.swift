ea7bcbb38aa45138e6ca26b30e30ad490b471c70
Scipioafricanus1 <bdharris3@uh.edu>
2017-11-27 13:35:41 -0600

    Revert "Attempt at fixing storyboard"
    This reverts commit ab2ab56b8440c0f5f9ba6f94a4fb81386918e4ad.

--- PowerPlant/ViewController.swift
+++ PowerPlant/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class ViewController: UIViewController {
 
     @IBAction func sequeButton(_ sender: UIButton) {
         performSegue(withIdentifier: "powerPlantToPlantTable", sender: self)
